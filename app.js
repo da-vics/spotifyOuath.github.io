5e2bb38090349387c25c0796b0953411d9f94c80
Tomoloju Victor <tomoloju45@gmail.com>
2021-07-29 02:13:45 -1100

    updates

--- app.js
+++ app.js
@@ -88,11 +88,11 @@ function handleAuthorizationResponse(){
         var data = JSON.parse(this.responseText);
         if ( data.access_token != undefined ){
             access_token = data.access_token;
-            // localStorage.setItem("access_token", access_token);
+            localStorage.setItem("access_token", access_token);
         }
         if ( data.refresh_token  != undefined ){
             refresh_token = data.refresh_token;
-            // localStorage.setItem("refresh_token", refresh_token);
+            localStorage.setItem("refresh_token", refresh_token);
         }
         onPageLoad();
     }
